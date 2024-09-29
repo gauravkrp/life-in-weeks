eec5d83ed2f42bf3f9197da2590be893c64ea88d
Gaurav Pandey <gaurav@getthera.com>
2024-09-29 05:54:14 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.ico" /> {/* Path to your favicon */}
+			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
 		</html>
 	);
