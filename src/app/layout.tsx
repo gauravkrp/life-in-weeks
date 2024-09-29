414790da8d59972f846eebf79f12560871c04295
Gaurav Pandey <gaurav@getthera.com>
2024-09-29 06:17:39 +0530

    og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,22 @@ export const metadata: Metadata = {
 		apple: "/icons/apple-touch-icon.png", // Optional apple touch icon
 	},
 	manifest: "/site.webmanifest", // Path to your manifest file
+	openGraph: {
+		title: "Your Life in Weeks | Count Your Life",
+		description: "Visualize Your Life in Weeks, Months, or Years: A Simple Life Planner",
+		url: "https://countyourlife.com", // Your app's URL
+		siteName: "Your Life in Weeks",
+		images: [
+			{
+				url: "/your-life-in-weeks.webp", // Path to your OG image
+				width: 1200,
+				height: 630,
+				alt: "Your Life in Weeks",
+			},
+		],
+		type: "website",
+		locale: "en_US",
+	},
 };
 
 export const viewport: Viewport = {
